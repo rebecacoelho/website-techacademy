4796de28dd21de15cdc318bf605ccd236e3dc0ef
rebecacoelho <rebecasantoscoelho@gmail.com>
2024-09-02 19:11:46 -0300

    remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
 import { SectionIntro } from "@/components/SectionIntro";
 import { SectionOurService } from "@/components/SectionOurService";
 import "./globals.css";
